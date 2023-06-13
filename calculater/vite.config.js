56a5a630961ca8800b7b4b5332948417ab2aceb9
Negar Baharmand <negarbaharmand@outlook.com>
2023-06-13 15:01:47 +0200

    typo

--- calculater/vite.config.js
+++ calculater/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/raect-calculator/",
+  base: "/react-calculator/",
   assetsDir: "calculater/src",
   plugins: [react()],
 });
