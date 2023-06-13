5d332e3665e0f55804e8fcd8452cfea3245a9591
Negar Baharmand <negarbaharmand@outlook.com>
2023-06-13 14:47:58 +0200

    amend assetsDir

--- calculater/vite.config.js
+++ calculater/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/raect-calculator/",
-  assetsDir: "assets",
+  assetsDir: "src",
   plugins: [react()],
 });
